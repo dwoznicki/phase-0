6cbe9546dc967c7ff62f7d16859ac8ec1e5b52ac
Daniel Woznicki <tokay89@sbcglobal.net>
2015-09-29 14:06:07 -0700

    Add leap-year

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,19 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def leap_year? year
+  if year%400 == 0
+    return true
+  end
+  if year%100 == 0
+    return false
+  end
+  if year%4 == 0
+    return true
+  else
+    return false
+  end
+end
