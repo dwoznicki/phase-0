e42c8a3a93d4247157bb38361aa9bc04b103105f
Daniel Woznicki <tokay89@sbcglobal.net>
2015-09-29 13:43:14 -0700

    Add triangle-side solution

---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,16 @@
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def valid_triangle?(a, b, c)
+  if a > 0 && b > 0 && c > 0
+    if (a + b > c)&&(a + c > b)&&(c + b > a)
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
+end
