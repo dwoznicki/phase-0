5968bcb22f516350e70bac6d87023d0391630eb1
Daniel Woznicki <tokay89@sbcglobal.net>
2015-10-02 11:30:24 -0700

    Add final solo challenges, minor fixes to pair programming files

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,14 @@
+# Factorial
+
+# I worked on this challenge with: Michael Jewsinski.
+
+
+# Your Solution Below
+def factorial(number)
+   pro= 1
+   until number == 0
+    pro = pro * number
+    number = number - 1
+   end
+   pro
+end
