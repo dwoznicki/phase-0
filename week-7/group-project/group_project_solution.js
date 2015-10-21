b7c4de48162e08d9876a2febc4a196a3a7cc2616
Daniel Woznicki <tokay89@sbcglobal.net>
2015-10-21 09:39:13 -0700

    Add user stories

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,8 @@
+/*
+=====User Stories=====
+1. As a user, I want the function "sum" to take a list of numbers, add them all together, and give me back the final sum. It should work for lists with both an even and odd length.
+
+2. As a user, I want the function "mean" to take a list of numbers, add them all together, divide by how many numbers there are in the list, and give back the answer. It should give back the average of the list. It should work for lists with both an even and odd length.
+
+3. As a user, I want the function "median" to take a list of numbers, find the middle value (or median value), and give it back. You can assume the list is sorted from smallest to largest. It should work for lists with both an even and odd length.
+*/
